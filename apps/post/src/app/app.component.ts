68a09db7f153a4484ad6062325dc5a8faa4e6428
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-01 17:17:08 +0100

    refactor(post): remove references to api interfaces

--- apps/post/src/app/app.component.ts
+++ apps/post/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@sample-angular-nest/api-interfaces';
 
 @Component({
   selector: 'sample-angular-nest-root',
@@ -8,6 +7,4 @@ import { Message } from '@sample-angular-nest/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
