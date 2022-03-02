0348e8dcf1897937e2ef7feab5e2342515658e4c
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-02 02:11:21 +0100

    feature: configure mongo in api app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ApiFeatureConfigModule } from '@sample-angular-nest/api/feature-config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoConfiguration, mongoConfiguration } from '@sample-angular-nest/api/utils-config';
 
 @Module({
-  imports: [ApiFeatureConfigModule],
+  imports: [
+    ApiFeatureConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [
+        mongoConfiguration.KEY
+      ],
+      useFactory: (config: MongoConfiguration) => {
+        return {
+          uri: config.uri,
+          dbName: config.dbName
+        }
+      }
+    })
+  ],
   controllers: [],
 })
 export class AppModule {}
