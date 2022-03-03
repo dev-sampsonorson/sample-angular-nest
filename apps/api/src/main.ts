07b4cabc32f2606da593248cc18f2746af00db94
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-03 09:05:08 +0100

    feat: add swagger configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,18 +2,39 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppConfiguration, appConfiguration } from '@sample-angular-nest/api/utils-config';
 
 import { AppModule } from './app/app.module';
+
+
+function configureSwagger(appConfig: AppConfiguration, app: INestApplication, globalPrefix: string) {
+    const swaggerDocOptions = new DocumentBuilder()
+        .setTitle('Nx Article API')
+        .setDescription('Nx Article API Docs')
+        .setVersion('1.0.0')
+        .addServer(appConfig.domain, 'development')
+        .build();
+
+    const swaggerDoc = SwaggerModule.createDocument(app, swaggerDocOptions);
+    const swaggerUiPath = `/${globalPrefix}/docs`;
+
+    SwaggerModule.setup(swaggerUiPath, app, swaggerDoc);
+
+    Logger.log(
+        `🚀 Swagger Docs enabled: ${appConfig.domain}${swaggerUiPath}`,
+        'NestApplication'
+    );
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<AppConfiguration>(appConfiguration.KEY);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+    app.setGlobalPrefix(globalPrefix);
+    configureSwagger(appConfig, app, globalPrefix);
   await app.listen(appConfig.port);
   Logger.log(
     `🚀 Listening on: ${appConfig.domain}/${globalPrefix}`
