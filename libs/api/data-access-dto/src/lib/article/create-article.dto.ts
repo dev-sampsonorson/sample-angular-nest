aa588a218b96565842292a7d191263e3e045e089
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-03 11:55:16 +0100

    feat: add create article endpoint to controller

--- libs/api/data-access-dto/src/lib/article/create-article.dto.ts
+++ libs/api/data-access-dto/src/lib/article/create-article.dto.ts
@@ -2,5 +2,5 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateArticleDto {
     @ApiProperty({ example: 'My First Article' }) text: string;
-    @ApiProperty({ example: 'Brandon' }) atuthor: string;
+    @ApiProperty({ example: 'Brandon' }) author: string;
 }
--- libs/api/feature-article/src/lib/article.controller.ts
+++ libs/api/feature-article/src/lib/article.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ArticleService } from '@sample-angular-nest/api/data-access-article';
+import { CreateArticleDto } from '@sample-angular-nest/api/data-access-dto';
 
 @Controller('articles')
 @ApiTags('articles')
@@ -13,5 +14,10 @@ export class ArticleController {
     @Get()
     async getAllArticles() {
         return await this._articleService.getAll();
+    }
+
+    @Post()
+    async createArticle(@Body() dto: CreateArticleDto) {
+        return await this._articleService.createArticle(dto);
     }
 }
