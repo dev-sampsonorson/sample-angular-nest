fe3fe1f28a05321482aaaaf5a596061e6a5571f7
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-03-03 12:17:40 +0100

    chore: remove @ApiProperty() form Dto and Schema, remove @ApiCreateRespone(...), and remove @ApiOkResponse(...)

--- libs/api/data-access-dto/src/lib/article/create-article.dto.ts
+++ libs/api/data-access-dto/src/lib/article/create-article.dto.ts
@@ -1,6 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateArticleDto {
+/* export class CreateArticleDto {
     @ApiProperty({ example: 'My First Article' }) text: string;
     @ApiProperty({ example: 'Brandon' }) author: string;
+} */
+
+export class CreateArticleDto {
+    text: string;
+    author: string;
 }
